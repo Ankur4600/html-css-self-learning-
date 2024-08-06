7c750c2ef3bf52759ea3fa9fc7044bcf59f21fd7
Ankur4600 <tinkushrivastav78@gmail.com>
2024-08-06 12:42:17 +0530

    css project 04

--- script/jsFirstr.js
+++ script/jsFirstr.js
@@ -1,2 +1,4 @@
 alert('welcome to our page');
-document.querySelector('button').onclick="(alert('counted'))";
+/* Note for me:- i need to learn how to add alert on clicking a button on this document.(means exact syntax to show alert on clicking the button)*/
+const button1=document.querySelector('button');
+button1.onclick="alert('hello')";
