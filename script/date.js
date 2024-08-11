44b3ec30132fe051e6cea00d850979be28e36f4e
Ankur4600 <tinkushrivastav78@gmail.com>
2024-08-11 13:15:34 +0530

    date in javaScript

---
+++ script/date.js
@@ -0,0 +1,11 @@
+//It create a new date object with the current date and time
+let date= new Date();
+console.log(date);
+//method to get time in milliseconds
+console.log(date.getTime());
+console.log(date.getFullYear());//gives 4 digit year
+console.log(date.getDay());//Day of week
+console.log(date.getMinutes());//current minutes
+console.log(date.getHours());//current hour
+console.log(date.getMonth());//current month
+console.log(date.getSeconds());//current second
